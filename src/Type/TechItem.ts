70085e503b545ff9868e8aeba8c70ae022cc0696
codewithalif <programmingwithalif@gmail.com>
2026-09-24 04:37:41 +0800

    All types are declaired in type folder

--- src/Type/TechItem.ts
+++ src/Type/TechItem.ts
@@ -1,10 +1,10 @@
-export interface ITechItem {
-  id: string;
-  name: string;
-  category: string;
-  description: string;
-  icon: string;
-  rating: number;
-  difficulty: 'Beginner-Friendly' | 'Intermediate' | 'Advanced' | string;
-  badge: string;
-}
+// export interface ITechItem {
+//   id: string;
+//   name: string;
+//   category: string;
+//   description: string;
+//   icon: string;
+//   rating: number;
+//   difficulty: 'Beginner-Friendly' | 'Intermediate' | 'Advanced' | string;
+//   badge: string;
+// }
