ddb753b8e779ff72dd7d8a9dffbdc757222aa8b1
codewithalif <programmingwithalif@gmail.com>
2026-09-24 04:33:54 +0800

    Technology-Card section task complete

--- src/Components/TechnologyCard.tsx
+++ src/Components/TechnologyCard.tsx
@@ -1,21 +1,21 @@
 
-// import type { ITechItem } from "../Type/TechItem";
-// import Card from "./Card";
-// interface TechnologyCardsProps {
-//   techData: ITechItem[];
-//   stackItems: ITechItem[];
-//   setStackItems: React.Dispatch<React.SetStateAction<ITechItem[]>>;
-// }
-// const TechnologyCards = ({ techData, stackItems, setStackItems }: TechnologyCardsProps) => {
+import type { ITechItem } from "../Type/TechItem";
+import Card from "./Card";
+interface TechnologyCardsProps {
+  techData: ITechItem[];
+  stackItems: ITechItem[];
+  setStackItems: React.Dispatch<React.SetStateAction<ITechItem[]>>;
+}
+const TechnologyCards = ({ techData, stackItems, setStackItems }: TechnologyCardsProps) => {
 
 
-//   return (
-//     <div className="grid md:grid-cols-3 gap-4">
-//       {techData.map((tech) => (
-//         <Card key={tech.id} tech={tech} stackItems={stackItems} setStackItems={setStackItems} />
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className="grid md:grid-cols-3 gap-4">
+      {techData.map((tech) => (
+        <Card key={tech.id} tech={tech} stackItems={stackItems} setStackItems={setStackItems} />
+      ))}
+    </div>
+  );
+};
 
-// export default TechnologyCards;
+export default TechnologyCards;
